c2cff181e983d9af8fa31810f650ba0e76bc0a79
patelrachith <patelrachith@gmail.com>
2022-08-04 18:32:03 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 export default function App() {
   return (
     <div>
-      <Router forceRefresh={true}>
+      <Router>
         <HeaderComponent />
         <div className="container">
           <Switch>
